66e9290890d01f4fa7525b2a657453ef0a847196
David Guerette <david.guerette@gmail.com>
2014-08-16 18:32:20 -0400

    Mouseover behavior targets divs with multiple classes.

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 $(document).ready(function() {
-    $('#box-1').mouseenter(function(){
-        $('#box-1').fadeTo('slow', 1);
+    $('.box.a').mouseenter(function(){
+        $('.box.a').fadeTo('slow', 1);
+    });
+    $('.box.a').mouseleave(function(){
+        $('.box.a').fadeTo('fast', 0.5);
     });
 
-    $('#box-1').mouseleave(function(){
-        $('#box-1').fadeTo('fast', 0.5);
+    $('.box.b').mouseenter(function(){
+        $('.box.b').fadeTo('fast', 1);
+    });
+    $('.box.b').mouseleave(function(){
+        $('.box.b').fadeTo('slow', 0.5);
     });
 
-    $('#box-2').mouseenter(function(){
-        $('#box-2').fadeTo('fast', 1);
+    $('.box.c').mouseenter(function(){
+      $('.box.c').fadeTo('slow', 1);
     });
 
-    $('#box-2').mouseleave(function(){
-        $('#box-2').fadeTo('slow', 0.5);
+    $('.box,c').mouseleave(function(){
+        $('.box.c').fadeTo('fast', 0.5);
     });
-});
+
+    $('.box.d').mouseenter(function(){
+        $('.box.d').fadeTo('fast', 1);
+    });
+    $('.box.d').mouseleave(function(){
+        $('.box.d').fadeTo('slow', 0.5);
+    });
+
+    $('.box.e').mouseenter(function(){
+        $('.box.e').fadeTo('fast', 1);
+    });
+    $('.box.e').mouseleave(function(){
+        $('.box.e').fadeTo('slow', 0.5);
+    });
+  });
